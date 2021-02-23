af0f49833ea772642d3113aa5e907b839e48d555
El Rafa <rafaelrosas19@gmail.com>
2021-02-23 15:34:28 -0600

    Created components directory and added nav component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
+import React from "react";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <nav class="navbar">
-        <div class="container d-flex justify-content-center">
-          <a class="navbar-brand ms-auto fas fa-users" href="#">
-            <span class="navemp"> Employee Directory </span>
-            <br></br>
-            <span class="navsub">
-              Click on carrots to filter by heading or use the search box to
-              narrow your results.
-            </span>
-          </a>
-        </div>
-      </nav>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav class="navbar">
+      <div class="container d-flex justify-content-center">
+        <a class="navbar-brand ms-auto fas fa-users" href="#">
+          <span class="navemp"> Employee Directory </span>
+          <br></br>
+          <span class="navsub">
+            Click on carrots to filter by heading or use the search box to
+            narrow your results.
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
