9bab9d8f17f6e3f0540beeabfc26189a128258d9
El Rafa <rafaelrosas19@gmail.com>
2021-03-01 13:49:12 -0600

    Changed the table to a material ui basic table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav";
 import UserTable from "./components/UserTable";
 import axios from "axios";
@@ -7,29 +7,21 @@ import axios from "axios";
 function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://randomuser.me/api/?results=200&nat=us").then((res) => {
-      const data = res.data.results;
-      setUsers([...data]);
-    });
-  }, []);
+  // useEffect(() => {
+  //   axios.get("https://randomuser.me/api/?results=10&nat=us").then((res) => {
+  //     const data = res.data.results;
+  //     setUsers([...data]);
+  //   });
+  // }, []);
 
   const handleClick = (event) => {
     event.preventDefault();
 
-    axios.get("https://randomuser.me/api/?results=200&nat=us").then((res) => {
+    axios.get("https://randomuser.me/api/?results=10&nat=us").then((res) => {
       const data = res.data.results;
       setUsers([...data]);
     });
   };
-
-  // console.log("users", users);
-
-  // const userList = users.map((user) => (
-  //   <p key={user.login.uuid}>
-  //     {user.name.first} {user.name.last}
-  //   </p>
-  // ));
 
   return (
     <div className="App">
